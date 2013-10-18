f65b0c128880fd1e76b6388068b24869fb00f699
Jorge Gómez Weyler <alu0100717723@ull.edu.es>
2013-10-18 10:55:27 +0100

    Implementación de los métodos de la clase Fraccion

---
+++ racional.rb
@@ -0,0 +1,72 @@
+# Implementar en este fichero la clase para crear objetos racionales
+
+require "./gcd.rb"
+
+class Fraccion
+	
+	
+	def initialize (a,b)
+		@a=a
+		@b=b
+	end	
+	
+	def to_s()
+		puts "#{@a}/#{@b}"
+	end
+	
+	def suma(numerador,denominador)
+		mcm= ((@b * denominador)/ gcd(@b, denominador))	#mcm(a,b)= (a*b)/ mcd(a,b)
+		numeradorAux= mcm/@b * @a 
+		numerador= mcm/denominador * numerador
+		sumaA= @a + numerador
+		sumaB=mcm
+		puts 'Suma:'
+		puts "#{sumaA}/#{sumaB}"
+		puts 'Suma simplificada:'
+		mcd= gcd(sumaA, sumaB)
+		sumaA= sumaA/mcd
+		sumaB= sumaB/mcd
+		puts "#{sumaA}/#{sumaB}"
+	end
+	
+	def resta(numerador, denominador)
+		mcm= ((@b * denominador)/ gcd(@b, denominador))	#mcm(a,b)= (a*b)/ mcd(a,b)
+		@a= mcm/@b * @a 
+		numeradorAux= mcm/denominador * numerador
+		restaA= @a - numerador
+		restaB=mcm
+		
+		puts 'Resta:'
+		puts "#{restaA}/#{restaB}"
+		puts 'Resta simplificada:'
+		mcd= gcd(restaA, restaB)
+		restaA= restaA/mcd
+		restaB= restaB/mcd
+		puts "#{restaA}/#{restaB}"
+		
+	end
+	
+	def producto(numerador, denominador)
+		productoA= @a * numerador
+		productoB= @b * denominador
+		puts 'Producto:'
+		puts "#{productoA}/#{productoB}"
+		puts 'Producto simplificada:'
+		mcd= gcd(productoA, productoB)
+		productoA= productoA/mcd
+		productoB= productoB/mcd
+		puts "#{productoA}/#{productoB}"
+	end
+	
+	def division (numerador, denominador)
+		numeradorAux= @a * denominador
+		denominadorAux= @b * numerador
+		puts 'División:'
+		puts "#{numeradorAux}/#{denominadorAux}"
+		puts 'Producto simplificada:'
+		mcd= gcd(numeradorAux, denominadorAux)
+		numeradorAux= numeradorAux/mcd
+		denominadorAux= denominadorAux/mcd
+		puts "#{numeradorAux}/#{denominadorAux}"
+	end
+end
